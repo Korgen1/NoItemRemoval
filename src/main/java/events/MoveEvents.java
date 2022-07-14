dcc4417f7f7e1cd0f388566759217567c69cf2bf
Korgen <korgen@korgen.xyz>
2022-07-14 03:59:39 -0400

    Added config and permissions

--- src/main/java/events/MoveEvents.java
+++ src/main/java/events/MoveEvents.java
@@ -1,5 +1,9 @@
 package events;
 
+import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryAction;
@@ -8,20 +12,44 @@ import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.inventory.Inventory;
+import xyz.korgen.noitemremoval.NoItemRemoval;
 
 public class MoveEvents implements Listener {
-
     @EventHandler
     public void OnItemDrop(PlayerDropItemEvent event){
-        event.setCancelled(true);
+        FileConfiguration config = NoItemRemoval.plugin.getConfig();
+        Player p = event.getPlayer();
+        if (!p.hasPermission("NoItemRemoval.Bypass")) {
+            for (String item : config.getStringList("whitelist")) {
+                if (event.getItemDrop().getItemStack().getType() == Material.matchMaterial(item)) {
+                    return;
+                }
+            }
+            event.setCancelled(true);
+        }
     }
     @EventHandler
     public void OnItemMove(InventoryClickEvent event){
-        if (event.getClickedInventory().getType() != InventoryType.PLAYER){
-            event.setCancelled(true);
-        }
-        if(event.getAction() == InventoryAction.MOVE_TO_OTHER_INVENTORY){
-            event.setCancelled(true);
+        FileConfiguration config = NoItemRemoval.plugin.getConfig();
+        HumanEntity p = event.getWhoClicked();
+        if (!p.hasPermission("NoItemRemoval.Bypass")) {
+            if (event.getClickedInventory().getType() != InventoryType.PLAYER) {
+                for (String item : config.getStringList("whitelist")) {
+                    if (event.getCurrentItem().getType() == Material.matchMaterial(item) ^ event.getCursor().getType() == Material.matchMaterial(item)) {
+                        return;
+                    }
+                }
+                event.setCancelled(true);
+            }
+            if (event.getAction() == InventoryAction.MOVE_TO_OTHER_INVENTORY) {
+                for (String item : config.getStringList("whitelist")) {
+                    if (event.getCurrentItem().getType() == Material.matchMaterial(item)) {
+                        return;
+                    }
+                }
+                event.setCancelled(true);
+            }
         }
     }
 }
+
--- src/main/java/xyz/korgen/noitemremoval/NoItemRemoval.java
+++ src/main/java/xyz/korgen/noitemremoval/NoItemRemoval.java
@@ -1,18 +1,35 @@
 package xyz.korgen.noitemremoval;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class NoItemRemoval extends JavaPlugin {
+
+    public static NoItemRemoval plugin;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         getServer().getPluginManager().registerEvents(new events.MoveEvents(), this);
 
+        List<String> list = new ArrayList<>();
+        list.add("DIRT");
+        list.add("COBBLESTONE");
+
+        FileConfiguration config = this.getConfig();
+        config.addDefault("whitelist", list);
+        config.options().copyDefaults(true);
+        saveConfig();
+
+        plugin = this;
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+
 }
